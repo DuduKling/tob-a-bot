92af86c66d17c8a52d4691508dfd46d35b8116cc
Eduardo Mesiano <eduardokmesiano@gmail.com>
2020-05-16 14:29:32 -0300

    First post route and nodemon
    Adding first post route with body-parser
    Adding nodemon to the dependencies

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+app.post('/post', (req, res) => {
+    console.log(req.body);
+    res.send('Post World!');
 });
 
 const serverPort = 3000;
